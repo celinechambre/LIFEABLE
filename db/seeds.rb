d3f0922bf3acaa14f42e81a5d42fc842f8a9a05e
raffaele triggiano <triggiano.r@gmail.com>
2020-09-09 14:53:00 +0200

    adding a comment about ADMIN TRUE inside teh seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -21,6 +21,9 @@ puts "..only if they are note commented out in the db:seeds file"
    last_name: "triggiano",
    username: "ChicoNico",
    admin: true
+   # the ADMIN field has been added to the USER TABLE via a migration.
+   # check migration file named ADD_ADMIN_TO_USER
+   # check the SCHEMA as well to see the prensence of that FIELD associated to the USER TABLE
  )
 
 # user = User.create!(
